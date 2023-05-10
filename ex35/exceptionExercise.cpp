c05b2ffd6794cd7622ba13b728d8c01a7b9adf97
markfaust123 <markfaust123@gmail.com>
2023-05-10 15:52:40 -0400

    completed exercise 35

--- ex35/exceptionExercise.cpp
+++ ex35/exceptionExercise.cpp
@@ -7,6 +7,11 @@
 /* Collect integers from a file; store them in a vector */
 std::vector<int> readFile(char* filename) {
   std::ifstream fin(filename);
+
+  if (!fin.is_open()) {
+    throw std::ios_base::failure("Error: failed to open specified input file.\n");
+  }
+
   std::vector<int> numbers(10);
 
   int n = 0;
@@ -19,12 +24,17 @@ std::vector<int> readFile(char* filename) {
     if (fin.fail()) { // if we failed to read an int, throw an exception
       throw std::invalid_argument("File contains non-integer data!\n");
     }
-    // otherwise, just add it into the vector
-    numbers.at(index) = n;
+    try {
+      // otherwise, just add it into the vector
+      numbers.at(index) = n;
+    } catch (std::out_of_range e) {
+      numbers.push_back(n);
+    }
     index++;
+    
   }
 
-  throw std::logic_error("ERROR: should never get here!");
+  throw std::logic_error("ERROR: should never get here!\n");
   return numbers;
 }
 
@@ -36,7 +46,19 @@ int main(int argc, char **argv) {
   }
 
   std::vector<int> numbers;
-  numbers = readFile(argv[1]);
+  try {
+    numbers = readFile(argv[1]);
+  } catch (std::out_of_range ex) {
+    std::cerr << "Error: input size out of vector range" << std::endl;
+    return 1;
+  } catch (std::ios_base::failure e) {
+    std::cout << e.what();
+    return 1;
+  } catch (std::invalid_argument e) {
+    std::cout << e.what();
+    return 1;
+  }
+  
 
   std::cout << "Read numbers: ";
   for(int &i : numbers) {
